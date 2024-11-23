7420ec903c93324571963c5c30e01f0386fae2ad
Vitor <vitor.suzarte@ecalc.com.br>
2024-11-23 14:50:09 -0300

    002 - Manter o sitema rodando depois de uma interação

--- loja.maga.console/loja.maga.console/Program.cs
+++ loja.maga.console/loja.maga.console/Program.cs
@@ -2,33 +2,38 @@
 {
     private static void Main(string[] args)
     {
-        Console.Clear();
-        Console.WriteLine("=========================================");
-        Console.WriteLine("          Bem-vindo à Loja de Mangas     ");
-        Console.WriteLine("=========================================");
-        Console.WriteLine("1. Área Administrativa");
-        Console.WriteLine("0. Sair");
-        Console.WriteLine("=========================================");
-        Console.Write("Escolha uma opção: ");
-        string opcao = Console.ReadLine();
+        bool finalizarSistema = false;
 
-        switch (opcao)
-        {
-            case "1":
-                Console.Clear();
-                Console.WriteLine("=========================================");
-                Console.WriteLine("          Área Administrativa            ");
-                Console.WriteLine("=========================================");
-                break;
+        //Enquanto for true, ele continua executando
+        while (!finalizarSistema) {
+            Console.Clear();
+            Console.WriteLine("=========================================");
+            Console.WriteLine("          Bem-vindo à Loja de Mangas     ");
+            Console.WriteLine("=========================================");
+            Console.WriteLine("1. Área Administrativa");
+            Console.WriteLine("0. Sair");
+            Console.WriteLine("=========================================");
+            Console.Write("Escolha uma opção: ");
+            string opcao = Console.ReadLine();
 
-            case "0":
-                Console.WriteLine("Saindo do sistema. Até logo!");
-                break;
+            switch (opcao)
+            {
+                case "1":
+                    Console.Clear();
+                    Console.WriteLine("=========================================");
+                    Console.WriteLine("          Área Administrativa            ");
+                    Console.WriteLine("=========================================");
+                    break;
 
-            default:
-                Console.WriteLine("Opção inválida! Pressione qualquer tecla para tentar novamente.");
-                Console.ReadKey();
-                break;
+                case "0":
+                    Console.WriteLine("Saindo do sistema. Até logo!");
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida! Pressione qualquer tecla para tentar novamente.");
+                    Console.ReadKey();
+                    break;
+            }
         }
     }
 }
